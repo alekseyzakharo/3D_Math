5b4de29588ffe20a8596b53fcf5d9d74f6d35aef
Aleksey Zakharov <azakharo@sfu.ca>
2018-05-08 13:19:41 -0700

    Initial Commit

---
+++ 3D_Math/Vector.cpp
@@ -0,0 +1,11 @@
+#include "Vector.h"
+
+Vector2d::Vector2d() {};
+
+Vector2d::Vector2d(float x, float y)
+{
+	Vector2d::x = x;
+	Vector2d::y = y;
+}
+
+
---
+++ 3D_Math/Vector.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Vector2d
+{
+public:
+	float x, y;
+	Vector2d();
+	Vector2d(float x, float y);
+
+
+};
+
+class Vector3d
+{
+
+};
+
+class Vector4d
+{
+
+};
